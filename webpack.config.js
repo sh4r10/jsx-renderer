abb6344efc5bc66547f13369568ca5a4d14fef06
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-01-29 00:57:29 +0100

    initial webpack setup
    setup the babel loader and the html plugin for webpack as well the dev-server

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/main.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "JSX Renderer",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
