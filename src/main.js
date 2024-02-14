8ae928004d4ddc34e5013e6fc2210f61398918da
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-02-15 04:36:34 +0500

    complete renderer with full implementation of the hyper function as well as the render function to successfully render the jsx

--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 /** @jsx hyper */
-let foo = <div id="foo">Hello!</div>;
+
+function hyper(name, attributes, ...args) {
+  const children = args.length ? [].concat(...args) : null;
+  const result = { name, attributes, children };
+  return result;
+}
+
+function render(virtualNode) {
+  if (virtualNode.split) return document.createTextNode(virtualNode);
+
+  const node = document.createElement(virtualNode.name);
+
+  const attributes = virtualNode.attributes || {};
+  Object.keys(attributes).forEach((key) =>
+    node.setAttribute(key, attributes[key]),
+  );
+
+  const children = virtualNode.children || [];
+  children.forEach((child) => node.appendChild(render(child)));
+
+  return node;
+}
+
+const parent = document.getElementById("root");
+
+let foo = <div id="foo">Hello from rendered jsx</div>;
+
+let res = render(foo);
+
+parent.appendChild(res);
