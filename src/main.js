a4ccb92a30c228923c4785b8ef7fc88e58ea303b
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-02-16 14:00:09 +0100

    Add element tree as well as support for styling, numbers and booleans
    Add an element tree to the right to visualize the entire process.
    Now numbers and booleans are rendered as text nodes.

--- src/main.js
+++ src/main.js
@@ -7,14 +7,25 @@ function hyper(name, attributes, ...args) {
 }
 
 function render(virtualNode) {
-  if (virtualNode.split) return document.createTextNode(virtualNode);
+  if (
+    typeof virtualNode == "string" ||
+    typeof virtualNode == "number" ||
+    typeof virtualNode == "boolean"
+  )
+    return document.createTextNode(virtualNode);
 
   const node = document.createElement(virtualNode.name);
 
   const attributes = virtualNode.attributes || {};
-  Object.keys(attributes).forEach((key) =>
-    node.setAttribute(key, attributes[key]),
-  );
+  Object.keys(attributes).forEach((key) => {
+    if (key == "style") {
+      Object.keys(attributes[key]).forEach((style) => {
+        node.style[style] = attributes[key][style];
+      });
+    } else {
+      node.setAttribute(key, attributes[key]);
+    }
+  });
 
   const children = virtualNode.children || [];
   children.forEach((child) => node.appendChild(render(child)));
@@ -22,10 +33,28 @@ function render(virtualNode) {
   return node;
 }
 
+const App = () => {
+  let value = 555;
+  return (
+    <div id="container">
+      <h1 class="heading">Welcome to JSX Renderer</h1>
+      <p>
+        Everything you see is rendered through <strong>JSX</strong>
+      </p>
+      <p>The element tree can be viewed on the right</p>
+      <p>This is a javascript variable, value = {value}</p>
+      <h2>Let's add some styling too</h2>
+      <p style={{ backgroundColor: "red" }}>This is red</p>
+    </div>
+  );
+};
+
 const parent = document.getElementById("root");
+const virtualDom = render(App());
+parent.appendChild(virtualDom);
 
-let foo = <div id="foo">Hello from rendered jsx</div>;
-
-let res = render(foo);
-
-parent.appendChild(res);
+let tree = JSON.stringify(App(), null, " ");
+// This demonstrates the entire process from JSX -> vDOM -> DOM
+document.body.appendChild(
+  render(<pre style={{ backgroundColor: "#eee" }}>{tree}</pre>),
+);
